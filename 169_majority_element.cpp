7d6819cd2e6ec2db8d4185fc26b4a5906bea5053
Karan Vardhan Raj <146817726+karan7955@users.noreply.github.com>
2026-08-07 15:44:38 +0530

    Add 169_majority_element.cpp - LeetCode Problem 169

---
+++ 169_majority_element.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        unordered_map <int,int> freq;
+        int n = nums.size();
+        for(auto num: nums){
+            freq[num]++;
+
+            if(freq[num]  > n / 2){
+                return num;
+            }
+        }
+        return -1;
+        
+    }
+};
